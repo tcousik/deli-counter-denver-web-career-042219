46a37aa2790a88c0a95f37d7822a4885f1af2399
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 21:50:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(names)
   if names.length == 0
     puts "The line is currently empty."
   else 
-    new_line = "The line is currently: "
+    new_line = "The line is currently:"
     names.each_with_index do |index, name|
     new_line << "#{index + 1}. #{name}."
   end
