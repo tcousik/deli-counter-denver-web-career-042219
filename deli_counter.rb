ee45ee64ecbede222b56cad6f741dafe1b937f23
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 21:45:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,9 @@ def line(names)
   if names.length == 0
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{katz_deli.length}. #{names}."
+    new_line = []
+    
+    
+    "The line is currently: #{katz_deli.length}. #{names}."
   end
 end
